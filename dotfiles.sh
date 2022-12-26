9ea6956965481b37c8ea9ebb6a0b6ca4abbafb9c
gvrDev <guilhermevrodrigues123@gmail.com>
2022-12-26 18:20:17 -0300

    dotfiles: added dotfiles standalone setup

---
+++ dotfiles.sh
@@ -0,0 +1,20 @@
+echo "Removing current dotfiles"
+rm -rf $HOME/.config/i3
+rm -rf $HOME/.config/nvim
+rm -rf $HOME/.config/kitty
+rm -rf $HOME/.config/my-autostart.sh
+rm -rf $HOME/.zshrc
+rm -rf $HOME/.p10k.zsh
+rm -rf $HOME/.my-dotfiles
+
+echo "Cloning new dotfiles"
+git clone https://github.com/gvrDev/dotfiles.git
+rm -rf $HOME/.my-dotfiles/
+mv dotfiles/ $HOME/.my-dotfiles/
+
+ln -sf $HOME/.my-dotfiles/i3 ~/.config
+ln -sf $HOME/.my-dotfiles/nvim ~/.config
+ln -sf $HOME/.my-dotfiles/kitty ~/.config
+ln -sf $HOME/.my-dotfiles/my-autostart.sh ~/.config
+ln -sf $HOME/.my-dotfiles/.zshrc ~/
+ln -sf $HOME/.my-dotfiles/.p10k.zsh ~/
--- setup.sh
+++ setup.sh
@@ -12,24 +12,5 @@ curl -o- https://raw.githubusercontent.com/gvrDev/bash-scripts/main/fonts.sh | b
 #zsh
 curl -o- https://raw.githubusercontent.com/gvrDev/bash-scripts/main/zsh.sh | bash
 
-
-echo "Removing current dotfiles"
-rm -rf $HOME/.config/i3
-rm -rf $HOME/.config/nvim
-rm -rf $HOME/.config/kitty
-rm -rf $HOME/.config/my-autostart.sh
-rm -rf $HOME/.zshrc
-rm -rf $HOME/.p10k.zsh
-rm -rf $HOME/.my-dotfiles
-
-echo "Cloning new dotfiles"
-git clone https://github.com/gvrDev/dotfiles.git
-rm -rf $HOME/.my-dotfiles/
-mv dotfiles/ $HOME/.my-dotfiles/
-
-ln -sf $HOME/.my-dotfiles/i3 ~/.config
-ln -sf $HOME/.my-dotfiles/nvim ~/.config
-ln -sf $HOME/.my-dotfiles/kitty ~/.config
-ln -sf $HOME/.my-dotfiles/my-autostart.sh ~/.config
-ln -sf $HOME/.my-dotfiles/.zshrc ~/
-ln -sf $HOME/.my-dotfiles/.p10k.zsh ~/
+#dotfiles
+curl -o- https://raw.githubusercontent.com/gvrDev/bash-scripts/main/dotfiles.sh | bash

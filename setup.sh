80dbc35ffbf0824b18e3911ebd4fb0eb1a9aeeaa
gvrDev <guilhermevrodrigues123@gmail.com>
2022-12-26 18:02:46 -0300

    fixed npm update call and refactored fonts install to curl fonts.sh

--- setup.sh
+++ setup.sh
@@ -18,13 +18,7 @@ nvm use --lts
 source ~/.bashrc
 
 echo "Installing fonts"
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Caskaydia Cove Nerd Font Complete Regular.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Regular.otf
-cd ~/.local/share/fonts && curl -fLo "Caskaydia Cove Nerd Font Complete Italic.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Italic.otf
-cd ~/.local/share/fonts && curl -fLo "Caskaydia Cove Nerd Font Complete Bold.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Bold/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Bold.otf
-cd ~/.local/share/fonts && curl -fLo "Caskaydia Cove Nerd Font Complete Bold Italic.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Bold/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Bold%20Italic.otf
-cd ~/.local/share/fonts && curl -fLo "Caskaydia Cove Nerd Font Complete Mono SemiBold Italic.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/SemiBold/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Mono%20SemiBold%20Italic.otf
-cd ~/.local/share/fonts && curl -fLo "Caskaydia Cove Nerd Font Complete Mono SemiBold.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/SemiBold/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Mono%20SemiBold.otf
+curl -o- https://raw.githubusercontent.com/gvrDev/bash-scripts/main/fonts.sh | bash
 
 echo "Removing oh-my-zsh plugins"
 find $HOME/.oh-my-zsh/custom/themes/ ! -name 'example.zsh-theme' -type f -exec rm -rf {} +
@@ -40,7 +34,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "Installing node dependencies"
-npm i -g update
+npm -g update
 npm i -g tree-sitter-cli
 
 echo "Removing current dotfiles"

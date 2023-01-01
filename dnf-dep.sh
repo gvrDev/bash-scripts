1741b3a88a651d5b3cce8136e59c835bab8a6a64
gvrDev <guilhermevrodrigues123@gmail.com>
2023-01-01 14:45:56 -0300

    feat(dnf-dep): Added polybar

--- dnf-dep.sh
+++ dnf-dep.sh
@@ -6,4 +6,4 @@ sudo dnf copr enable atim/lazygit -y
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
-sudo dnf install -y neovim kitty zsh lazygit brave-browser xclip xset xinput ripgrep fd-find exa g++ gamemode openssh-askpass flameshot
+sudo dnf install -y neovim kitty zsh lazygit brave-browser xclip xset xinput ripgrep fd-find exa g++ gamemode openssh-askpass flameshot polybar
